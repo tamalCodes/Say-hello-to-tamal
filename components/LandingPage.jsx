277d22be2a20141c109b9babf40c29700f394aab
Tamal Das <gyansujan69@gmail.com>
2022-05-06 14:56:36 +0530

    fix: Updated Desktop UI (#3)

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -2,22 +2,61 @@ import React from 'react'
 import styles from "../styles/Landing.module.css"
 import Image from "next/image"
 import pimg from "../public/assets/mobport.svg"
-import pimg2 from "../public/assets/grp5.png"
+import pimg2 from "../public/assets/tamalport1.png"
 import Navbar from './Navbar'
+import Link from 'next/link'
 
 const LandingPage = () => {
     return (
         <div className={styles.parent}>
 
-            <Navbar />
-
-            <div className={styles.landing_imgdiv}>
-
-
-                <Image src={pimg2} width={480} height={520} alt="portfolio" className={styles.portfolioimg} />
-
-
+            <div className={styles.navbardiv}>
+                <Navbar />
             </div>
+
+            <div className={`container ${styles.parentDesktop}`}>
+                <div className="row">
+
+                    <div className={`col-8`}>
+                        <div className={styles.landingheydesktop}>
+
+                            <div className={styles.landing_navbar}>
+                                <p className={styles.navlink1}>Projects</p>
+                                <p className={styles.navlink2}>About me</p>
+                                <p className={styles.navlink3}>Contact me</p>
+                            </div>
+
+                            <p className={styles.landing_heythere}>Hey there, <br /> I&apos;m Tamal</p>
+
+                            <p className={styles.landing_heythere2}>A passionate Web developer from India, <br /> contributing to open source and connecting communities !!  <br /> <br /> Look at my projects, know more about to me and feel free to reach out !!  </p>
+
+                            <button className={`btn btn-warning ${styles.explorebtn}`}>
+                                Explore &gt;
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className={`col-4`}>
+                        <div className={styles.landing_imgdiv}>
+
+
+                            <Image src={pimg2} width={350} height={350} alt="portfolio" className={styles.portfolioimg} />
+
+
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+
+
+
+
+
+
 
 
             <div className={styles.landing_imgdiv2}>
