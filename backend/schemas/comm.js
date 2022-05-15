360205277d9ba0dd774a0a6cf9c10cd6145f7282
Tamal Das <gyansujan69@gmail.com>
2022-05-16 01:01:43 +0530

    Added communities

---
+++ backend/schemas/comm.js
@@ -0,0 +1,30 @@
+export default {
+  name: "comms",
+  title: "Comms",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "imgUrl",
+      title: "ImgUrl",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "link",
+      title: "Link",
+      type: "string",
+    },
+  ],
+};
---
+++ src/container/Communities/Communities.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable quotes */
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-quotes */
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import './About.scss';
+import { urlFor, client } from '../../client';
+
+const Communities = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text"> Giving back to   <span>the community 💙 </span> <br /></h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="abthead" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="abtdesc" style={{ marginTop: 25 }}>{about.description}</p>
+            <p className="abtdesc" style={{ marginTop: 10, color: "black", fontWeight: "600" }}>Hello</p>
+
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Communities, 'app__about'),
+  'about',
+  'app__whitebg',
+);
