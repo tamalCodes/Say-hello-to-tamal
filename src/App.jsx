58678f9cae26adb0cd693e74d68b9fbe0d7102d6
Tamal <gyansujan69@gmail.com>
2023-03-24 14:26:44 +0530

    fix: adding meeting links

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable quotes */
-import React from "react";
+import React, { useEffect } from "react";
 
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 import "./App.scss";
 import Communities from "./container/Communities/Communities";
 
-const App = () => (
-  <div className="app">
-    <Navbar />
-    <Header />
-    <About />
-    <Work />
-    <Skills />
-    <Communities />
-    <Testimonial />
-    <Footer />
-  </div>
-);
+const App = () => {
+
+
+
+  useEffect(() => {
+    if (window.location.pathname === "/meet") {
+      window.location.href = "https://meet.google.com/bwr-rhma-exk";
+    }
+  }, []);
+
+
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Communities />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
+
+
