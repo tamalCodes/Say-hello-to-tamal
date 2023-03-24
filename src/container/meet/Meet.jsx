c7d8d61158ebc204bfcbae4c30f6c8c45b1c422c
Tamal <gyansujan69@gmail.com>
2023-03-24 16:23:45 +0530

    fix: Changes to meeting time

--- src/container/meet/Meet.jsx
+++ src/container/meet/Meet.jsx
@@ -1,16 +1,19 @@
+/* eslint-disable react/jsx-indent */
 import React, { useEffect } from 'react';
 import { Navbar } from '../../components';
 
 const Meet = () => {
     useEffect(() => {
         if (window.location.pathname === '/meet') {
-            window.location.href = 'https://meet.google.com/bwr-rhma-exk';
+            setTimeout(() => {
+                window.location.href = 'https://meet.google.com/bwr-rhma-exk';
+            }, 2000);
         }
     }, []);
 
     return (
         <>
-            <div style={{ textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", margin: "auto", justifyItems: 'center', fontWeight: "600", fontFamily: "sans-serif", fontSize: "3rem", height: "100vh" }}>
+            <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', margin: 'auto', justifyItems: 'center', fontWeight: '600', fontFamily: 'sans-serif', fontSize: '3rem', height: '100vh' }}>
                 <p> Loading your meeting 🚀 </p>
             </div>
 
