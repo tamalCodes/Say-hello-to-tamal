5f6fc97baa1084c6b3c8342beee3fbce66ef0acc
Tamal Das <gyansujan69@gmail.com>
2023-05-01 11:01:00 +0530

    feat: updated links

--- src/container/resume/Resume.jsx
+++ src/container/resume/Resume.jsx
@@ -8,7 +8,7 @@ const Resume = () => {
     useEffect(() => {
         if (window.location.pathname === '/resume') {
             setTimeout(() => {
-                window.location.href = 'https://drive.google.com/file/d/1N6SaRyXLIb9117wbCjlBiqOoadYAKzOw/view?usp=sharing';
+                window.location.href = 'https://drive.google.com/file/d/1wZTqr5hlDmVqAnbidVrNfNtrdwJnznI3/view?usp=sharing';
             }, 500);
         }
     }, []);
